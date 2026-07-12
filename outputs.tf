2de4e7b381d6934fb254634cb397ec67dc436908
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:00:41 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "automation_powershell72_modules_id" {
+  description = "Map of id values across all automation_powershell72_modules, keyed the same as var.automation_powershell72_modules"
+  value       = { for k, v in azurerm_automation_powershell72_module.automation_powershell72_modules : k => v.id }
+}
 output "automation_powershell72_modules_automation_account_id" {
   description = "Map of automation_account_id values across all automation_powershell72_modules, keyed the same as var.automation_powershell72_modules"
   value       = { for k, v in azurerm_automation_powershell72_module.automation_powershell72_modules : k => v.automation_account_id }
