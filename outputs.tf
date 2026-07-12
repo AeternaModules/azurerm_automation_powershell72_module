8e3722c5a9c0960ef395e614b2c6b3a84288083a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "automation_powershell72_modules" {
-  description = "All automation_powershell72_module resources"
-  value       = azurerm_automation_powershell72_module.automation_powershell72_modules
-}
 output "automation_powershell72_modules_automation_account_id" {
-  description = "List of automation_account_id values across all automation_powershell72_modules"
-  value       = [for k, v in azurerm_automation_powershell72_module.automation_powershell72_modules : v.automation_account_id]
+  description = "Map of automation_account_id values across all automation_powershell72_modules, keyed the same as var.automation_powershell72_modules"
+  value       = { for k, v in azurerm_automation_powershell72_module.automation_powershell72_modules : k => v.automation_account_id }
 }
 output "automation_powershell72_modules_module_link" {
-  description = "List of module_link values across all automation_powershell72_modules"
-  value       = [for k, v in azurerm_automation_powershell72_module.automation_powershell72_modules : v.module_link]
+  description = "Map of module_link values across all automation_powershell72_modules, keyed the same as var.automation_powershell72_modules"
+  value       = { for k, v in azurerm_automation_powershell72_module.automation_powershell72_modules : k => v.module_link }
 }
 output "automation_powershell72_modules_name" {
-  description = "List of name values across all automation_powershell72_modules"
-  value       = [for k, v in azurerm_automation_powershell72_module.automation_powershell72_modules : v.name]
+  description = "Map of name values across all automation_powershell72_modules, keyed the same as var.automation_powershell72_modules"
+  value       = { for k, v in azurerm_automation_powershell72_module.automation_powershell72_modules : k => v.name }
 }
 output "automation_powershell72_modules_tags" {
-  description = "List of tags values across all automation_powershell72_modules"
-  value       = [for k, v in azurerm_automation_powershell72_module.automation_powershell72_modules : v.tags]
+  description = "Map of tags values across all automation_powershell72_modules, keyed the same as var.automation_powershell72_modules"
+  value       = { for k, v in azurerm_automation_powershell72_module.automation_powershell72_modules : k => v.tags }
 }
 
